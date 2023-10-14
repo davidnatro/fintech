9f9d0fb4e67b1fb4e304a3356c19abd50650318d
David <davidnatro03@gmail.com>
2023-10-14 09:20:47 +0600

    configuration for homework 5

--- src/main/java/com/fintech/fintech/controller/advice/CommonExceptionAdvice.java
+++ src/main/java/com/fintech/fintech/controller/advice/CommonExceptionAdvice.java
@@ -5,23 +5,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpServerErrorException;
 
 @RestControllerAdvice
 public class CommonExceptionAdvice {
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(HttpClientErrorException.class)
-    public ExceptionModel handleHttpClientError(HttpClientErrorException exception) {
-        return new ExceptionModel(exception.getMessage(), HttpStatus.BAD_REQUEST.value());
-    }
-
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler(HttpServerErrorException.class)
-    public ExceptionModel handleHttpServerError(HttpServerErrorException exception) {
-        return new ExceptionModel(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value());
-    }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalArgumentException.class)
